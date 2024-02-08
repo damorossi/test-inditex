691e78cf6cf8321452b9f19b3626a09819810f8b
Damian <damianrossi@live.com.ar>
2024-02-08 11:57:28 +0100

    Installed Sass, a color pallete and created a layout page

---
+++ src/components/LayoutComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ListComponent from '../../../components/ListComponent';
+
+import './layout.scss';
+
+const LayoutComponent = (props) => {
+	return (
+		<div className="main">
+			<h1>List </h1>
+		</div>
+	);
+};
+
+export default LayoutComponent;
